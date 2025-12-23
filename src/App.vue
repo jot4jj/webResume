f0fcf10e232d7a462a4c0b7974d0ad7951ba8fb5
jot4jj <jsilvafreitas25@gmail.com>
2025-12-23 06:58:48 -0300

    feat(app): Adiciona estrutura base do App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+  import HeroSection from './components/HeroSection.vue';
+  import ExperienceTimeline from './components/ExperienceTimeline.vue';
+  import Stack from './components/Stack.vue';
+  import ProjectCard from './components/ProjectCard.vue';
+  import Menu from './components/Menu.vue';
 </script>
 
 <template>
-</template>
+  <div class="min-h-screen flex flex-col items-center z-50">
+    <div class="my-4">
+      <Menu />
+    </div>
 
+    <main class="container justify-center place-items-center mx-auto space-y-16 w-3/4">
+      <HeroSection/>
+      <ExperienceTimeline/>
+      <Stack />
+      <ProjectCard />
+    </main>
+  </div>
+</template>
